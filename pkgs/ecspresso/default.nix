36f388f8a3331d9233e978b2879fca11283f50f6
takeokunn <bararararatty@gmail.com>
2024-01-28 11:57:16 +0900

    Add ecspresso

--- pkgs/ecspresso/default.nix
+++ pkgs/ecspresso/default.nix
@@ -1,1 +1,34 @@
+{ lib
+, buildGoModule
+, fetchFromGitHub
+}:
 
+buildGoModule rec {
+  pname = "ecspresso";
+  version = "2.3.1";
+
+  src = fetchFromGitHub {
+    owner = "kayac";
+    repo = "ecspresso";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-3u+WXEvuG/LCZG9qk8sGmITql1o00xZpGNtXmup6cdQ=";
+  };
+
+  vendorHash = "sha256-WyjKkNtfZRJUARcn5xdL0MvkhQc0UZORicl8VcpdA1M=";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X=main.Version=v${version}"
+  ];
+
+  subPackages = [ "cmd/ecspresso" ];
+
+  meta = with lib; {
+    description = "A deployment tool for Amazon ECS";
+    homepage = "https://github.com/kayac/ecspresso";
+    license = licenses.mit;
+    maintainers = with maintainers; [ natsukium ];
+    mainProgram = "ecspresso";
+  };
+}
