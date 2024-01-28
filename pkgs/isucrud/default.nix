a2457e6424843c5538fae5bbe0ac29ece3a066c6
takeokunn <bararararatty@gmail.com>
2024-01-28 12:57:56 +0900

    Add isucrud

---
+++ pkgs/isucrud/default.nix
@@ -0,0 +1,27 @@
+{ buildGoModule
+, fetchFromGitHub
+}:
+
+buildGoModule rec {
+  pname = "isucrud";
+  version = "1.1.0";
+
+  src = fetchFromGitHub {
+    owner = "mazrean";
+    repo = "isucrud";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-f5a684d0bf17fc9a02bfc748ca19cc160e9bba19";
+  };
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X=main.Version=v${version}"
+  ];
+
+  meta = {
+    description = "ISUCON用DBへのCRUDへのデータフロー可視化ツール";
+    homepage = "https://github.com/mazrean/isucrud";
+    mainProgram = "isucrud";
+  };
+}
