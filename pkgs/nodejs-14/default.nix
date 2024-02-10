933ab807514185295ed2aa8cc0efc2cc4f1ac802
takeokunn <bararararatty@gmail.com>
2024-02-10 18:38:41 +0900

    update

--- pkgs/nodejs-14/default.nix
+++ pkgs/nodejs-14/default.nix
@@ -1,14 +1,13 @@
 { callPackage, python310, lib, stdenv, openssl, enableNpm ? true }:
-
 let
-  buildNodejs = callPackage ./nodejs.nix {
+  buildNodejs = callPackage <nixpkgs/pkgs/development/web/nodejs/nodejs.nix> {
     inherit openssl;
     python = python310;
   };
 in
-buildNodejs {
-  inherit enableNpm;
-  version = "14.21.3";
-  sha256 = "sha256-RY7AkuYK1wDdzwectj1DXBXaTHuz0/mbmo5YqZ5UB14=";
-  patches = lib.optional stdenv.isDarwin ./bypass-xcodebuild.diff;
-}
+  buildNodejs {
+    inherit enableNpm;
+    version = "14.21.3";
+    sha256 = "sha256-RY7AkuYK1wDdzwectj1DXBXaTHuz0/mbmo5YqZ5UB14=";
+    # patches = lib.optional stdenv.isDarwin ./bypass-xcodebuild.diff;
+  }
